5bb0d639dc53c7cb00f51bc670f46d97d9c9f3ed
wbjari <wbjari@gmail.com>
2016-05-11 12:19:39 +0200

    Material design package toegevoegd + master layout aangemaakt

---
+++ webservice/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="container">
+    <div class="content">
+      <div class="title">Homepagina</div>
+    </div>
+  </div>
+@endsection
---
+++ webservice/resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="assets/css/material-kit.css.css" media="screen" charset="utf-8">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
