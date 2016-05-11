dd035734f5481996ba3cb5baf64c5b0550c53d10
koentjeh <koen-debont@hotmail.com>
2016-05-11 14:30:17 +0200

    homepagina + logo

--- webservice/resources/views/home.blade.php
+++ webservice/resources/views/home.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="container">
-    <div class="content">
-      <div class="title">Homepagina</div>
-    </div>
-  </div>
+
+<div class="header-big">
+	<div class="container">
+		<img src="assets/img/logo-white.png" class="logo">
+
+		<div class="nav-menu">
+			<a href="#aanmelden"><li>Aanmelden</li></a>
+			<a href="#inloggen"><li>inloggen</li></a>
+		</div>
+	</div>
+</div>
+
+<div class="jumbotron">
+	
+</div>
+
+<div class="container">
+	<div class="content">
+		<div class="title">Homepagina</div>
+	</div>
+</div>
+
+
 @endsection
--- webservice/resources/views/layouts/master.blade.php
+++ webservice/resources/views/layouts/master.blade.php
@@ -5,7 +5,8 @@
 
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen" charset="utf-8">
-        <link rel="stylesheet" href="assets/css/material-kit.css.css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="assets/css/material-kit.css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="assets/css/core.css" media="screen" charset="utf-8">
     </head>
     <body>
         @yield('content')
