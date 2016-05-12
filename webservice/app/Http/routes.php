e68202967019bb76f5a57e5658e4ebf8d1aff5a5
wbjari <wbjari@gmail.com>
2016-05-12 09:50:55 +0200

    Registratie pagina aangemaakt

--- webservice/app/Http/routes.php
+++ webservice/app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
---
+++ webservice/resources/views/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container-fluid">
+  <div class="blue-top"></div>
+</div>
+
+@endsection
