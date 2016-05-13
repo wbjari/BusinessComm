a7957f8a0c27cfebd29d5a54cd029f93f6a78e4a
wbjari <wbjari@gmail.com>
2016-05-13 11:07:34 +0200

    Registratie pagina update

--- webservice/resources/views/register.blade.php
+++ webservice/resources/views/register.blade.php
@@ -3,18 +3,56 @@
 @section('content')
 
   <div class="page-lr" id="register">
-    <div class="lr-block">
-      <div class="col-sm-12">
-	       <div class="form-group label-floating">
-		         <label class="control-label">With Floating Label</label>
-		        <input type="email" class="form-control">
-	      </div>
-      </div>
+    <div class="block-lr">
+      <form class="register-form">
 
-      <button class="btn btn-primary btn-raised btn-fab btn-round form-submit">
-        <i class="material-icons">favorite</i>
+        <div class="col-sm-6">
+  	       <div class="form-group label-floating">
+  		         <label class="control-label" name="firstname">Voornaam *</label>
+  		         <input type="text" class="form-control" name="firstname">
+  	      </div>
+        </div>
+
+        <div class="col-sm-6">
+  	       <div class="form-group label-floating">
+  		         <label class="control-label" name="lastname">Achternaam *</label>
+  		         <input type="text" class="form-control" name="lastname">
+  	      </div>
+        </div>
+
+        <div class="col-sm-12">
+  	       <div class="form-group label-floating">
+  		         <label class="control-label" name="email">E-mailadres *</label>
+  		         <input type="text" class="form-control" name="email">
+  	      </div>
+        </div>
+
+        <div class="col-sm-6">
+  	       <div class="form-group label-floating">
+  		         <label class="control-label" name="password">Wachtwoord *</label>
+  		         <input type="text" class="form-control" name="password">
+  	      </div>
+        </div>
+
+        <div class="col-sm-6">
+  	       <div class="form-group label-floating">
+  		         <label class="control-label" name="password-repeat">Herhaal wachtwoord *</label>
+  		         <input type="text" class="form-control" name="password-repeat">
+  	      </div>
+        </div>
+
+      <button class="btn btn-danger btn-raised btn-fab btn-round form-submit btn-lr">
+        <i class="material-icons">forward</i>
       </button>
+
+      <div class="clearfix"></div>
+
+    </form>
+    <div class="col-sm-12">
+      <span class="required-lr">Velden met een * zijn verplicht.</span>
     </div>
+    </div>
+    <span>hallo</span>
   </div>
 
 @endsection
