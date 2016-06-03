e14c53835e4fb44b5f07e189c1a8895decf10851
wbjari <wbjari@gmail.com>
2016-06-03 11:30:05 +0200

    TokenMismatchException error gefixt

--- webservice/app/Http/routes.php
+++ webservice/app/Http/routes.php
@@ -10,49 +10,53 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(['middleware' => 'web'], function () {
 
-Route::get('/', function () {
-  if (Auth::guest()) {
-    return view('home');
-  } else {
-    return redirect('/dashboard');
-  }
-});
+  Route::get('/', function () {
+    if (Auth::guest()) {
+      return view('home');
+    } else {
+      return redirect('/dashboard');
+    }
+  });
 
-Route::auth();
+  Route::auth();
 
-// Everything below the following route is only accessable when logged in.
-Route::group(['middleware' => 'auth'], function () {
+  // Everything below the following route is only accessable when logged in.
+  Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('/dashboard', 'DashboardController@index');
+      Route::get('/dashboard', 'DashboardController@index');
 
-    Route::get('/admin', 'AdminController@index');
-    Route::get('/admin/company/report/confirm/{report_id}', 'AdminController@company_confirm_report');
-    Route::get('/admin/company/report/delete/{report_id}', 'AdminController@company_delete_report');
-    Route::get('/admin/company/activate/{company_id}', 'AdminController@company_activate');
-    Route::get('/admin/user/report/confirm/{report_id}', 'AdminController@user_confirm_report');
-    Route::get('/admin/user/report/delete/{report_id}', 'AdminController@user_delete_report');
-    Route::get('/admin/user/activate/{user_id}', 'AdminController@user_activate');
+      Route::get('/admin', 'AdminController@index');
+      Route::get('/admin/company/report/confirm/{report_id}', 'AdminController@company_confirm_report');
+      Route::get('/admin/company/report/delete/{report_id}', 'AdminController@company_delete_report');
+      Route::get('/admin/company/activate/{company_id}', 'AdminController@company_activate');
+      Route::get('/admin/user/report/confirm/{report_id}', 'AdminController@user_confirm_report');
+      Route::get('/admin/user/report/delete/{report_id}', 'AdminController@user_delete_report');
+      Route::get('/admin/user/activate/{user_id}', 'AdminController@user_activate');
 
-    Route::post('/company/report', 'CompanyController@report_company');
-    Route::any('/company/{company_id}/edit', 'CompanyController@edit');
-    Route::get('/company/{company_id}', [ 'as' => 'company', 'uses' => 'CompanyController@index']);
+      Route::post('/company/report', 'CompanyController@report_company');
+      Route::any('/company/{company_id}/edit', 'CompanyController@edit');
+      Route::get('/company/{company_id}', [ 'as' => 'company', 'uses' => 'CompanyController@index']);
 
-    Route::get('/create-company', 'CompanyController@index_create');
-    Route::post('/create-company', 'CompanyController@create');
+      Route::get('/create-company', 'CompanyController@index_create');
+      Route::post('/create-company', 'CompanyController@create');
 
-    Route::any('/change-logo', 'CompanyController@change_logo');
+      Route::any('/change-logo', 'CompanyController@change_logo');
 
-    Route::post('/request', 'CompanyController@request');
-    Route::post('/cancel-request', 'CompanyController@cancel_request');
-    Route::post('/accept-request', 'CompanyController@accept_request');
-    Route::post('/deny-request', 'CompanyController@deny_request');
+      Route::post('/request', 'CompanyController@request');
+      Route::post('/cancel-request', 'CompanyController@cancel_request');
+      Route::post('/accept-request', 'CompanyController@accept_request');
+      Route::post('/deny-request', 'CompanyController@deny_request');
 
-    Route::post('/create-post', 'PostController@create');
-    Route::post('/remove-post', 'PostController@remove');
+      Route::post('/create-post', 'PostController@create');
+      Route::post('/remove-post', 'PostController@remove');
 
 
-    Route::post('/user/report', 'UserController@report_user');
-    Route::any('/user/{user_id}/edit', 'UserController@edit');
-    Route::get('/user/{user_id}', [ 'as' => 'user', 'uses' => 'UserController@index']);
+      Route::post('/user/report', 'UserController@report_user');
+      Route::any('/user/{user_id}/edit', 'UserController@edit');
+      Route::get('/user/{user_id}', [ 'as' => 'user', 'uses' => 'UserController@index']);
+
+  });
+
 });
